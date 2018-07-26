8305774292e9b8b2934630b4a43d599fc9f4e06b
Sergey Sokolov <serjihsklovski@gmail.com>
2018-07-26 04:55:13 +0300

    test runner; 'SampleCountArray' tests; 'CMakeList.txt': test target.

---
+++ src/test/c/test_runner.c
@@ -0,0 +1,11 @@
+#include <api/sample_count_array_api_impl_test.h>
+#include <lib/test_utils.h>
+
+int main(int argc, char* argv[]) {
+    ADD_TEST(SampleCountArrayApiImpl__push_back__test_1);
+    ADD_TEST(SampleCountArrayApiImpl__shrink_to_fit__test_1);
+    ADD_TEST(SampleCountArrayApiImpl__push_back__test_2);
+    ADD_TEST(SampleCountArrayApiImpl__end__test_1);
+
+    test_utils()->run_tests();
+}
