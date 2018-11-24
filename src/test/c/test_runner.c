621182d364c960caac7eab416463e74812f9dd5a
Davenolt <davenolt.sh@gmail.com>
2018-11-24 04:49:08 +0300

    adding double-array domain, api, its implementation and tests. methods: init, del, shrink_to_fit, resize, push_back, push_some begin, end

--- src/test/c/test_runner.c
+++ src/test/c/test_runner.c
@@ -1,7 +1,17 @@
+#include <api/double_array_api_impl_test.h>
 #include <api/uint64_array_api_impl_test.h>
 #include <tiny_test/test_utils.h>
 
 int main(int argc, char* argv[]) {
+    // DoubleArrayApiImpl Tests
+    ADD_TEST(DoubleArrayApiImpl__push_back__test_1);
+    ADD_TEST(DoubleArrayApiImpl__shrink_to_fit__test_1);
+    ADD_TEST(DoubleArrayApiImpl__shrink_to_fit__test_2);
+    ADD_TEST(DoubleArrayApiImpl__push_back__test_2);
+    ADD_TEST(DoubleArrayApiImpl__push_some__test_3);
+    ADD_TEST(DoubleArrayApiImpl__end__test_1);
+
+    // Uint64ArrayApiImpl Tests
     ADD_TEST(Uint64ArrayApiImpl__push_back__test_1);
     ADD_TEST(Uint64ArrayApiImpl__shrink_to_fit__test_1);
     ADD_TEST(Uint64ArrayApiImpl__push_back__test_2);
