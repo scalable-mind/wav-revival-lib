6a8fd5a849c26b25a81f66de3fdfb1075095268c
Davenolt <davenolt.sh@gmail.com>
2018-12-10 21:41:29 +0300

    Removed commented code from `compress_smooth_int16_array` in WavCompressingUtils.

--- src/main/c/util/wav_compressing_utils.c
+++ src/main/c/util/wav_compressing_utils.c
@@ -15,7 +15,7 @@ static void compress_int16_array(int16_t data[], size_t data_size, bool start_ch
     for (size_t i = 0; i < data_size; i++) {
         norm_val = (int8_t) (abs(data[i]) > threshold ? 1 : 0);
 
-        if(start_chunk) {
+        if (start_chunk) {
             dst->start_value = norm_val;
             start_chunk = false;
         } else if (*prev_val != norm_val) {
@@ -38,7 +38,7 @@ static void compress_smooth_int16_array(int16_t data[], size_t data_size, bool s
     for (size_t i = 0; i < data_size; i++) {
         norm_val = (int8_t) (abs(data[i]) > threshold ? 1 : 0);
 
-        if(start_chunk) {
+        if (start_chunk) {
             dst->start_value = norm_val;
             start_chunk = false;
         } else if (*prev_val != norm_val) {
@@ -47,27 +47,9 @@ static void compress_smooth_int16_array(int16_t data[], size_t data_size, bool s
                  && dst->compressed_data->size > 0)) {
                 *(uint64_array_api().end(dst->compressed_data) - 1) += *samples_count;
                 *fill_ones = !(*fill_ones);
-            }
-            else {
+            } else {
                 uint64_array_api().push_back(dst->compressed_data, *samples_count);
             }
-//            if (*prev_val == 1) {                                                          // !A
-//                if(*fill_ones) {                                                           // B
-//                    *(uint64_array_api().end(dst->compressed_data) - 1) += *samples_count; // a
-//                    *fill_ones = !(*fill_ones);                                            // b
-//                }
-//                else {
-//                    uint64_array_api().push_back(dst->compressed_data, *samples_count);
-//                }
-//            } else if (dst->compressed_data->size > 0 && *prev_val == 0) {                 // A && D
-//                if(*samples_count > silence) {                                             // C
-//                    uint64_array_api().push_back(dst->compressed_data, *samples_count);    // c
-//                }
-//                else {
-//                    *(uint64_array_api().end(dst->compressed_data) - 1) += *samples_count; // a
-//                    *fill_ones = !(*fill_ones);                                            // b
-//                }
-//            }
 
             *samples_count = 0;
         }
