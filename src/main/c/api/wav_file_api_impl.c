f8b8956badc271edffc3af7342b7e8e7ddfae23e
Davenolt <davenolt.sh@gmail.com>
2019-02-07 21:30:39 +0300

    Refactor: removed FileIoMode, signatures changed to const char*

--- src/main/c/api/wav_file_api_impl.c
+++ src/main/c/api/wav_file_api_impl.c
@@ -5,15 +5,15 @@
 #include <api/wav_file_api.h>
 #include <domain/wav_file.h>
 
-static WavFile* init(const char* file_path, FileIoMode file_io_mode, size_t buffer_size) {
+static WavFile* init(const char* file_path, const char* file_io_mode, size_t buffer_size) {
     WavFile* wav_file = malloc(sizeof(WavFile));
     wav_file->buffer_size = buffer_size;
 #if _WIN32 || _WIN64
     //TODO: file_io_mode validation
     //TODO: errno validation
-    errno_t err = fopen_s(&(wav_file->file), file_path, file_io_mode == READ ? "rb" : "wb");
+    errno_t err = fopen_s(&(wav_file->file), file_path, file_io_mode);
 #else
-    wav_file->file = fopen(file_path, file_io_mode == READ ? "rb" : "wb");
+    wav_file->file = fopen(file_path, file_io_mode);
 #endif
     fread(&(wav_file->header), sizeof(WavHeader), 1, wav_file->file);
     return wav_file;
